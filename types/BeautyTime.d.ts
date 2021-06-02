01933c3fcbf763d373f8a336c20df8ca7fc3204d
zitronenclara <clara.hertel@outlook.de>
2021-06-02 18:12:06 +0200

    tsc and npm shenanies

---
+++ types/BeautyTime.d.ts
@@ -0,0 +1,52 @@
+export = BeautyTime;
+/**
+ * BeautyTime is a universal timespan format and time calculating utility
+ *
+ * @class
+ * @constructor
+ * @public
+ */
+declare class BeautyTime {
+    /**
+     * Creates an instace of BeautyTime
+     *
+     * @since 1.0.0
+     * @param {number} milliseconds The amount of milliseconds or the unix timestamp
+     * @param {number} seconds The amount of seconds
+     * @param {number} minutes The amount of minutes
+     * @param {number} hours The amount of hours
+     * @param {number} days The amount of days
+     */
+    constructor(milliseconds?: number, seconds?: number, minutes?: number, hours?: number, days?: number);
+    /**
+     * time is the length of the timespan in milliseconds
+     *
+     * @type {number}
+     * @public
+     */
+    public time: number;
+}
+declare namespace BeautyTime {
+    export { fac };
+}
+/**
+ * The calculation factors for the different time units
+ */
+type fac = {
+    /**
+     * factor of milliseconds to seconds
+     */
+    s: number;
+    /**
+     * factor of milliseconds to minutes
+     */
+    m: number;
+    /**
+     * factor of milliseconds to hours
+     */
+    h: number;
+    /**
+     * factor of milliseconds to days
+     */
+    d: number;
+};
